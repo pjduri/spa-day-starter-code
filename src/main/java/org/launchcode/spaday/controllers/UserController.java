3b5f3e2f4ff783f6592d0a8961342ce6df11996e
Your Name <pjduri@gmail.com>
2022-10-06 21:44:49 -0500

    studio from 10/6/22 completed with group

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,33 @@
+
+package org.launchcode.spaday.controllers;
+
+import org.launchcode.spaday.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("add")
+    String displayAddUserForm() {
+        return "user/add";
+    }
+
+    @PostMapping("add")
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute(user);
+        if(user.getPassword().equals(verify)) {
+            return "user/index";
+        }
+        model.addAttribute("error", "Passwords Don't Match!");
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+        return "user/add";
+        }
+
+}
