86af266587b39c6c689b37e4c64e2ad4a1326865
Your Name <pjduri@gmail.com>
2022-10-06 22:42:26 -0500

    re-factored process and display user form methods to use RequestMapping and point to the same path allowing for the form to be submitted again after a user has attempted to submit non-matching passwords or empty fields, with appropriate error messages displayed

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -4,30 +4,49 @@ package org.launchcode.spaday.controllers;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping("add")
+    @RequestMapping(value = "add", method = {RequestMethod.GET})
     String displayAddUserForm() {
         return "user/add";
     }
 
-    @PostMapping("add")
+    @RequestMapping(value = "/add", method = {RequestMethod.POST})
     public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
         model.addAttribute(user);
-        if(user.getPassword().equals(verify)) {
+        // returns the service selection page
+        if (user.getPassword().equals(verify)
+                && !user.getPassword().equals(null)
+                && !user.getPassword().equals("")) {
             return "user/index";
+        // returns add page with username and email fields still filled in
+        } else if (!user.getPassword().equals(verify)) {
+            model.addAttribute("error", "Passwords Don't Match!");
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
         }
-        model.addAttribute("error", "Passwords Don't Match!");
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
+        //  changes error return if there are empty fields
+        else if ((user.getPassword().equals("")
+                || user.getPassword().equals(null))
+                || verify.equals("")
+                || verify.equals(null)
+                || user.getUsername().equals("")
+                || user.getUsername().equals(null)
+                || user.getEmail().equals("")
+                || user.getEmail().equals(null)) {
+            model.addAttribute("error", "All Fields Required!");
+            model.addAttribute("username", user.getUsername());
+            model.addAttribute("email", user.getEmail());
+        }
         return "user/add";
-        }
+
+    }
 
 }
+
+
+
